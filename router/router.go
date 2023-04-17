c2baa1b15c88ed9ecd49c13967eb8841cde936df
Riley Grotenhuis <rg@buildmidwestern.com>
2023-04-16 21:20:27 -0500

    feat: add basic middleware

--- router/router.go
+++ router/router.go
@@ -1,10 +1,15 @@
 package router
 
 import (
+	"go-fiber-url-shortener/middleware"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func Initialize(router *fiber.App) {
+	router.Use(middleware.Security)
+	router.Use(middleware.Json)
+
 	router.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).SendString("Hello World!")
 	})
