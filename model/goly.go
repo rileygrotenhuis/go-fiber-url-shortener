f83d338c244e8b334ca55310f78fb2e079c54a4f
Riley Grotenhuis <rg@buildmidwestern.com>
2023-04-16 21:32:25 -0500

    fix: add short_url database field

--- model/goly.go
+++ model/goly.go
@@ -2,6 +2,7 @@ package model
 
 type Goly struct {
 	ID        int    `gorm:"primaryKey" json:"id"`
+	ShortUrl  string `gorm:"unique" json:"shortUrl"`
 	FullUrl   string `gorm:"unique" json:"fullUrl"`
 	CreatedAt int64  `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt int64  `gorm:"autoUpdateTime" json:"updatedAt"`
