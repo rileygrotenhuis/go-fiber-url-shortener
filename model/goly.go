d08f0b9239761d77730ab87410e8dbdfd5ec188f
Riley Grotenhuis <rg@buildmidwestern.com>
2023-04-16 21:50:02 -0500

    fix: make fields required

--- model/goly.go
+++ model/goly.go
@@ -2,8 +2,8 @@ package model
 
 type Goly struct {
 	ID        int    `gorm:"primaryKey" json:"id"`
-	ShortUrl  string `gorm:"unique" json:"shortUrl"`
-	FullUrl   string `gorm:"unique" json:"fullUrl"`
+	ShortUrl  string `gorm:"unique;not null" json:"shortUrl"`
+	FullUrl   string `gorm:"unique;not null" json:"fullUrl"`
 	CreatedAt int64  `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt int64  `gorm:"autoUpdateTime" json:"updatedAt"`
 }
