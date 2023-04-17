4f12b297890b6897bba4a9c5b68769e2f9b73166
Riley Grotenhuis <rg@buildmidwestern.com>
2023-04-16 22:10:47 -0500

    feat: setup create goly logic

--- handlers/goly.go
+++ handlers/goly.go
@@ -1,9 +1,34 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"go-fiber-url-shortener/database"
+	"go-fiber-url-shortener/model"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func CreateGoly(c *fiber.Ctx) error {
-	return nil
+	createNewGolyRequest := new(model.Goly)
+
+	var bodyParserError error = c.BodyParser(createNewGolyRequest)
+
+	if bodyParserError != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "invalid incoming request values",
+		})
+	}
+
+	var databaseError error = database.DB.Create(createNewGolyRequest).Error
+
+	if databaseError != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "failed to create Goly",
+		})
+	}
+
+	return c.Status(201).JSON(fiber.Map{
+		"message": "success",
+	})
 }
 
 func UpdateGoly(c *fiber.Ctx) error {
